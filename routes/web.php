bbc6cc7889f63adbb3e57d6d0237db1bc7601513
Arif Firmansyah <64146@hq.bni.co.id>
2025-03-21 09:59:42 +0700

    up:add #1

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::middleware(['auth'])->group(function ($e) {
+    Route::get('/', function () {
+        return redirect('/extraweb/login');
+    });
+    
+    Route::group(['prefix' => 'extraweb'], function ($e) {
+        Route::get('/', 'App\Http\Controllers\Backend\UserController@login')->name('extraweb.login');
+        Route::get('/login', 'App\Http\Controllers\Backend\UserController@login')->name('extraweb.login2');
+        Route::get('/logout', 'App\Http\Controllers\Backend\UserController@logout')->name('extraweb.logout');
+        Route::get('/forgot-password', 'App\Http\Controllers\Backend\AuthController@forgot_password')->name('extraweb.forgot_password');
+        Route::get('/register', 'App\Http\Controllers\Backend\AuthController@register')->name('extraweb.register');
+        
+        
+    });
 });
